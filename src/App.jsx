24f6f5efbc23321ddb7de8927227538e706fb501
edupoly <info.edupoly@gmail.com>
2023-04-25 07:56:06 +0530

    first day

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Child from "./Child";
+
+function App() {
+  return (
+   <div className="border border-2 m-2 p-2">
+    <h1>Welcome to Edupoly</h1>
+    <Child></Child>
+   </div>
+  );
+}
+
+export default App;
+//check you have nodejs
+// npx create-react-app dasuapp
+// goto the dasuapp folder
+// goto terminal in that folder 
+// run the commonad npm start
---
+++ src/Child.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Child(){
+    return(
+        <div className='border border-2 m-2 p-2'>
+            <h2>Child Component</h2>
+        </div>
+    )
+}
+export default Child;
